21df5724b73793272a697fc9c92ba30aaaa3ef84
Nicolas Portier <nicola.portier@gmail.com>
2025-03-03 16:28:02 +0100

    add: characters controller

--- controllers/charactersController.js
+++ controllers/charactersController.js
@@ -1,20 +1,62 @@
 const Character = require('../models/characters');
+const { body, validationResult } = require('express-validator');
+const mongoose = require('mongoose');
 
-const createCharacter = async ({ user_id, race, gender, avatar }) => {
-    try {
-        const newCharacter = await Character.create({
-            user_id,
-            race,
-            gender,
-            avatar
-        });
+/**
+ * Règles de validation des personnages
+ */
+const characterValidationRules = () => [
+    body('user', 'User ID is required').notEmpty(),
+    body('race', 'Race ID is required').notEmpty(),
+    body('gender', 'Gender is required').notEmpty().isIn(['female', 'male', 'non-binary']),
+];
 
-        await newCharacter.save();
-
-        return newCharacter;
-    } catch (error) {
-        throw new Error("Erreur lors de la création du personnage : " + error.message);
+/**
+ * Middleware pour valider les requêtes de création de personnage
+ */
+const validateCharacter = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ success: false, message: 'Validation failed', errors: errors.array() });
     }
+    next();
 };
 
-module.exports = createCharacter;
+const createCharacter = async ({ user, race, gender, avatar = '' }) => {
+    if (!user || !race || !gender) {
+        throw new Error('Missing required fields for character creation');
+    }
+
+    // Vérifie si l'utilisateur a déjà un personnage
+    const existingCharacter = await Character.findOne({ user });
+    if (existingCharacter) {
+        throw new Error('User already has a character');
+    }
+
+    return Character.create({ user, race, gender, avatar });
+};
+
+const createCharacterFromSignup = async (userData) => {
+    const { _id: user, race = new mongoose.Types.ObjectId(), gender, avatar } = userData;
+
+    if (!user || !race || !gender) {
+        throw new Error('Missing required fields for character creation');
+    }
+
+    const existingCharacter = await Character.findOne({ user });
+    if (existingCharacter) {
+        throw new Error('User already has a character');
+    }
+
+    return Character.create({ user, race, gender, avatar });
+    // return createCharacter({ user, race, gender, avatar });
+};
+
+console.log(new mongoose.Types.ObjectId());
+
+module.exports = {
+    characterValidationRules,
+    validateCharacter,
+    createCharacter,
+    createCharacterFromSignup
+};
