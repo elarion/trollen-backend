ec9fdb5f83db833d66b226e38a472927f61bb790
Nicolas Portier <nicola.portier@gmail.com>
2025-03-04 15:44:07 +0100

    fix: characters routes

--- controllers/charactersController.js
+++ controllers/charactersController.js
@@ -2,23 +2,54 @@ const Character = require('../models/characters');
 const { validationResult } = require('express-validator');
 const mongoose = require('mongoose');
 const characterValidationRules = require('../validators/characterValidator');
+
+
 /**
- * Middleware to validate character data
+ * Get all characters by user ID
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ * @returns 
  */
-const validateCharacter = (req, res, next) => {
-    const errors = validationResult(req);
+const getAllCharactersByUserId = async (req, res, next) => {
+    const { userId } = req.params;
 
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ success: false, message: 'Validation failed', errors: errors.array() });
+    try {
+        const characters = await Character.find({ user: userId });
+
+        return res.json({ success: true, characters });
+    } catch (error) {
+        next(error);
     }
+}
 
-    next();
-};
+/**
+ * Get character by user ID
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ * @returns 
+ */
+const getCharacterByUserId = async (req, res, next) => {
+    const { characterId, userId } = req.params;
+
+    try {
+        const character = await Character.findOne({ _id: characterId, user: userId });
+
+        if (!character) throw { statusCode: 404, message: 'Character not found' };
+
+        return res.json({ success: true, character });
+    } catch (error) {
+        next(error);
+    }
+}
 
 /**
  * Create a character
  */
-const createCharacter = async ({ user, race, gender, avatar = '' }) => {
+const createCharacter = async (req, res, next) => {
+    const { user, race = new mongoose.Types.ObjectId(), gender, avatar = '' } = req.body;
+
     try {
         // check if user already has a character
         const existingCharacter = await Character.findOne({ user });
@@ -26,27 +57,30 @@ const createCharacter = async ({ user, race, gender, avatar = '' }) => {
             throw { statusCode: 409, message: 'The user already has a character' };
         }
 
-        const character = await Character.create({ user, race, gender, avatar });
-        return character;
+        const newCharacter = new Character({ user, race, gender, avatar });
+        await newCharacter.save();
+
+        return res.json({ success: true, character: newCharacter });
     } catch (error) {
-        throw error;
+        next(error);
     }
 };
 
-const createCharacterFromSignup = async (userData) => {
-    const { _id: user, race = new mongoose.Types.ObjectId(), gender, avatar = '' } = userData;
-
+const createCharacterFromSignup = async ({ _id: user, race = new mongoose.Types.ObjectId(), gender, avatar = '' }) => {
     const existingCharacter = await Character.findOne({ user });
-    if (existingCharacter) {
-        throw { statusCode: 409, message: 'The user already has a character' };
-    }
+    if (existingCharacter) throw { statusCode: 409, message: 'The user already has a character' };
 
-    return await Character.create({ user, race, gender, avatar });
+    const newCharacter = new Character({ user, race, gender, avatar });
+    await newCharacter.save();
+
+    return newCharacter;
 };
+
 
 module.exports = {
     characterValidationRules,
-    validateCharacter,
     createCharacter,
-    createCharacterFromSignup
+    createCharacterFromSignup,
+    getCharacterByUserId,
+    getAllCharactersByUserId
 };
