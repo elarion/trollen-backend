a9c97ab06f51a546a773ca2f2dc3e9e7330e9f2f
Alej-o <lejour.agathe@outlook.fr>
2025-03-05 15:42:07 +0100

    joinParty

--- routes/parties.js
+++ routes/parties.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const {allParties,joinPartyById} = require("../controllers/partiesController");
+const {allParties,partyById, joinPartyById} = require("../controllers/partiesController");
 const errorHandler = require("../middlewares/errorHandler")
 const validateRequest = require("../middlewares/validationRequest")
+router.use(errorHandler);
+
 
 router.get("/", validateRequest, allParties),
-router.get("/id", validateRequest, partyById),
+router.get("/:id", validateRequest, partyById),
+router.put("/:id", validateRequest,joinPartyById);
 
 
-router.use(errorHandler);
+
 module.exports = router;
