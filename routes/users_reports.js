f3026dc6989992513d4338fb45735f59077b0d70
Your name <Your email>
2025-03-10 17:20:46 +0100

    report

--- routes/users_reports.js
+++ routes/users_reports.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const {reportUser} = require("../controllers/usersReportsController");
 const errorHandler = require("../middlewares/errorHandler")
+const authenticateToken = require("../middlewares/authenticateToken")
 // const validateRequest = require("../middlewares/validationRequest")
 router.use(errorHandler);
 
-router.post("/:id", reportUser),
+router.post("/:id", authenticateToken, reportUser),
 
 module.exports = router;
