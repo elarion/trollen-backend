f4bf52571549bf22045adcb88dac99f152164f8b
pierre <pierre.boisnard@live.fr>
2025-03-10 15:06:44 +0100

    update route, controller and services for adding friend

--- services/userFriendService.js
+++ services/userFriendService.js
@@ -1,7 +1,17 @@
-const addFriend = async () => {
+const UserFriend = require('../models/users_friends')
 
+const add = async ({user_1, user_2}) => {
+    const friend = await UserFriend.findOne({$or : [{user_1 , user_2}, {user_1 : user_2, user_2 : user_1}]});
+    // Checker en BDD si y'a pas déjà cette amitié, or 
+    // UserFriend.findOne({$or : [{user_1 , user_2}, {user_1 : user_2, user_2 : user_1}])
+    // On peut enregister la friendship
+    const newFriend = new UserFriend({
+        user_1,
+        user_2
+    })
+    await newFriend.save();
 }
 
 module.exports = {
-    addFriend
+    add
 }
