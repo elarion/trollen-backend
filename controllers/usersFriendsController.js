6e8b81508cddd31e34528f42501f5bbeefaf55ff
pierre <pierre.boisnard@live.fr>
2025-03-10 16:30:42 +0100

    update usersFriendsController

--- controllers/usersFriendsController.js
+++ controllers/usersFriendsController.js
@@ -4,7 +4,6 @@ const addFriend = async (req, res, next) => {
     try {
         const user_1 = req.user._id;
         const user_2 = req.body.targetUserId;
-        // const { username } = req.body;
         
         const userFriend = await userFriendService.add({user_1, user_2})
 
